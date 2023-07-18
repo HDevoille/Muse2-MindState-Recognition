a44ae4788966b8eb543f7afc8926165fbaceb65c
HDevoille <113013403+HDevoille@users.noreply.github.com>
2023-07-18 14:31:18 +0200

    Update EEG_DL.py

--- Python files/EEG_DL.py
+++ Python files/EEG_DL.py
@@ -68,32 +68,39 @@ Global_Data['F4'] = [Gamma_mean for i in range(len(Global_Data))]
 # Data_subject3 = pd.concat([Focus_subject3,Relax_subject3],ignore_index=True)
 
 # X = np.concatenate((X_subject0_scaled,X_subject1_scaled))
-X = Global_Data[['Delta','Theta','Alpha','Beta','Gamma','F1','F2','F3','F4']]
+X = Global_Data[['Delta','Theta','Alpha','Beta','Gamma']]
 # X = preprocessing.scale(X)
 
 y = Global_Data[['label']]
 
-# norm_l = tf.keras.layers.Normalization(axis=-1)
-# norm_l.adapt(X)  # learns mean, variance
-# X = norm_l(X)
+norm_l = tf.keras.layers.Normalization(axis=-1)
+norm_l.adapt(X)  # learns mean, variance
+X = norm_l(X)
 
-# X_t = np.tile(X,(100,1))
-# y_t = np.tile(y,(100,1))
+X_t = np.tile(X,(100,1))
+y_t = np.tile(y,(100,1))
+
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_binary_accuracy',restore_best_weights=True,baseline=0.70)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(25,activation='relu',name='layer1'),
-    tf.keras.layers.Dense(15,activation='relu',name='layer2'),
-    tf.keras.layers.Dense(1,activation='sigmoid',name='output')
+    # tf.keras.layers.Dense(500,activation='relu'),
+    tf.keras.layers.Dense(275,activation='relu'),
+    tf.keras.layers.Dense(150,activation='relu'),
+    tf.keras.layers.Dense(25,activation='relu'),
+    tf.keras.layers.Dense(1,activation='sigmoid')
 ])
 
 model.compile(
     loss = tf.keras.losses.BinaryCrossentropy(),
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.01),
+    metrics= tf.keras.metrics.BinaryAccuracy()
 )
 
 model.fit(
-    X,y,            
-    epochs=200,
+    X_t,y_t,            
+    epochs=10,
+    validation_split= 0.2,
+    callbacks = [callback]
 )
 
 F_test = pd.read_csv("Dataset/F_test.csv")
@@ -102,17 +109,21 @@ F_test['F2'] = [F_test['Gamma'][i] + F_test['Beta'][i] for i in range(len(F_test
 F_test['F3'] = [F_test['Alpha'][i] - F_test['Gamma'][i] + F_test['Beta'][i] for i in range(len(F_test))]
 Gamma_mean = np.mean(F_test['Gamma'])
 F_test['F4'] = [Gamma_mean for i in range(len(F_test))]
-X_new = F_test[['Delta','Theta','Alpha','Beta','Gamma','F1','F2','F3','F4']]
+F_test['label'] = [ 1 for i in range(len(F_test))]
+X_new = F_test[['Delta','Theta','Alpha','Beta','Gamma']]
+y_new = F_test[['label']]
 
-prediction = model.predict(X_new)
-count = 0
-for i in prediction:
-    if i > 0.5:
-        count += 1
+X_new = norm_l(X_new)
 
-print(prediction)
-print(count)
-print(count/180)
+# prediction = model.predict(X_new)
+# count = 0
+# for i in prediction:
+#     if i > 0.5:
+#         count += 1
+
+# # print(prediction)
+# print(count)
+# print(count/180)
 
 R_test = pd.read_csv("Dataset/R_test.csv")
 R_test['F1'] = [R_test['Gamma'][i]**3 for i in range(len(R_test))]
@@ -120,14 +131,28 @@ R_test['F2'] = [R_test['Gamma'][i] + R_test['Beta'][i] for i in range(len(R_test
 R_test['F3'] = [R_test['Alpha'][i] - R_test['Gamma'][i] + R_test['Beta'][i] for i in range(len(R_test))]
 Gamma_mean = np.mean(R_test['Gamma'])
 R_test['F4'] = [Gamma_mean for i in range(len(R_test))]
-X_new2 = R_test[['Delta','Theta','Alpha','Beta','Gamma','F1','F2','F3','F4']]
+R_test['label'] = [ 0 for i in range(len(R_test))]
+X_new2 = R_test[['Delta','Theta','Alpha','Beta','Gamma']]
 
-prediction2 = model.predict(X_new2)
-count2 = 0
-for i in prediction2:
-    if i < 0.5:
-        count2 += 1
+X_new2 = norm_l(X_new2)
+y_new2 = R_test[['label']]
 
-print(prediction2)
-print(count2)
-print(count2/180)
+Global_Test_Data = pd.concat([F_test,R_test])
+
+X_test = Global_Test_Data[['Delta','Theta','Alpha','Beta','Gamma']]
+y_test = Global_Test_Data[['label']]
+
+norm_l.adapt(X_test)
+X_test = norm_l(X_test)
+
+print("Test accuracy : ",model.evaluate(X_test,y_test)[1])
+
+# prediction2 = model.predict(X_new2)
+# count2 = 0
+# for i in prediction2:
+#     if i < 0.5:
+#         count2 += 1
+
+# # print(prediction2)
+# print(count2)
+# print(count2/180)
